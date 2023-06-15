d197dc42951559fb3ee9092e3a2b8c9e425d8694
ineyon <114392029+ineyon@users.noreply.github.com>
2023-06-15 07:38:50 +0300

    Виправлений алгоритм пошуку зв'язки

--- Crypt.O.py
+++ Crypt.O.py
@@ -7,6 +7,10 @@ from tkinter import ttk, font, Canvas, Entry
 # створення вікна
 window = tk.Tk()
 
+# функція кнопки
+def calculate_button():
+    getBestChainFunc()
+
 window.title('CRYPT.o')
 
 window.geometry('892x553')
@@ -18,7 +22,7 @@ window.configure(background='#2B2B2B')
 
 customFont20 = font.Font(family='Chakra Petch', size=20)
 customFont15 = font.Font(family='Chakra Petch', size=15)
-customFont10 = font.Font(family='Chakra Petch', size=10)
+customFont10 = font.Font(family='Chakra Petch', size=8)
 
 # створення заокругленого квадрату для інтерфейсу
 
@@ -61,8 +65,8 @@ rectangleToPanel = round_rectangle(29,286,231,439, radius=28, fill="#363636")
 rectangleToCoin = round_rectangle(41,327,219,367, radius=28, fill="#FF9900")
 rectangleToPrice = round_rectangle(41,377,219,417, radius=28, fill="#535353")
 
-rectangleCalcucateButton = round_rectangle(29,468,231,526, radius=28, fill="#FF9900")
-rectangleCalcucateResult = round_rectangle(253,468,869,526, radius=28, fill="#363636")
+rectangleCalculateButton = round_rectangle(29,468,231,526, radius=28, fill="#FF9900")
+rectangleCalculateResult = round_rectangle(253,468,869,526, radius=28, fill="#363636")
 
 rectanglePricePanel = round_rectangle(253,73,869,439, radius=28, fill="#363636")
 
@@ -71,6 +75,9 @@ rectangleCoinPanel2 = round_rectangle(281,164,841,217, radius=28, fill="#535353"
 rectangleCoinPanel3 = round_rectangle(281,230,841,283, radius=28, fill="#535353")
 rectangleCoinPanel4 = round_rectangle(281,296,841,349, radius=28, fill="#535353")
 rectangleCoinPanel5 = round_rectangle(281,362,841,415, radius=28, fill="#535353")
+
+button = tk.Button(window, text="", width=25, height=3, bg="#FF9900", relief=tk.FLAT, highlightthickness=0, command=calculate_button)
+button.place(x=38, y=470)
 
 labelCoin1 = ttk.Label(master=window, text = 'Updating...', font =customFont10, background='#535353', foreground='white')
 labelCoin1.place(x=295, y=111)
@@ -87,12 +94,12 @@ labelProgramName = ttk.Label(master=window, text = 'CRYPT.o', font =customFont20
 labelProgramName.place(x=80, y=18)
 labelFromText = ttk.Label(master=window, text = 'From', font =customFont15, background='#363636', foreground='white')
 labelFromText.place(x=105, y=74)
-labelFromText = ttk.Label(master=window, text = 'To', font =customFont15, background='#363636', foreground='white')
-labelFromText.place(x=117, y=286)
-labelFromText = ttk.Label(master=window, text = 'Calculate', font =customFont15, background='#FF9900', foreground='white')
-labelFromText.place(x=89, y=476)
-labelFromText = ttk.Label(master=window, text = 'Best chain :', font =customFont10, background='#363636', foreground='white')
-labelFromText.place(x=295, y=483)
+labelToText = ttk.Label(master=window, text = 'To', font =customFont15, background='#363636', foreground='white')
+labelToText.place(x=117, y=286)
+labelCalculateText = ttk.Label(master=window, text = 'Calculate', font =customFont15, background='#FF9900', foreground='white')
+labelCalculateText.place(x=89, y=476)
+labelBestChainText = ttk.Label(master=window, text = 'Best chain :', font =customFont10, background='#363636', foreground='white')
+labelBestChainText.place(x=295, y=483)
 
 entryFromCoin = Entry(window, background='#FF9900',foreground='white',justify='center', borderwidth=0, font=customFont15,width=12)
 entryFromCoin.place(x=50,y=115)
@@ -123,33 +130,45 @@ coins = ['btc', 'eth', 'doge', 'ltc', 'bch']
 exchanges = ['binance', 'coinbase']
 sides = ['buy', 'sell']
 
-best_coin_pair = ''
-best_percentage = 0
-best_buy_exchange = ''
-best_sell_exchange = ''
-best_buy_price = 0
-best_sell_price = 0
+
 
 def getBestChainFunc():
+    
+    best_coin_pair = ''
+    best_percentage = 0.00
+    best_buy_exchange = ''
+    best_sell_exchange = ''
+    best_buy_price = 0
+    best_sell_price = 0
+    
     for coin in coins:
         for buy_exchange in exchanges:
             for sell_exchange in exchanges:
-                for buy_side in sides:
-                    for sell_side in sides:
-                        if buy_exchange == sell_exchange and buy_side == sell_side:
-                            continue
-                        buy_price = get_coin_price(coin, buy_exchange, buy_side)
-                        sell_price = get_coin_price(coin, sell_exchange, sell_side)
-                        if buy_price is None or sell_price is None:
-                            continue
-                        percentage = (sell_price - buy_price) / buy_price * 100
-                        if percentage > best_percentage:
-                            best_coin_pair = coin.upper()
-                            best_percentage = percentage
-                            best_buy_exchange = buy_exchange.capitalize()
-                            best_sell_exchange = sell_exchange.capitalize()
-                            best_buy_price = buy_price
-                            best_sell_price = sell_price
+                if buy_exchange == sell_exchange:
+                    continue
+                buy_price = get_coin_price(coin, buy_exchange, 'buy')
+                sell_price = get_coin_price(coin, sell_exchange, 'sell')
+                if buy_price is None or sell_price is None:
+                    continue
+                percentage = (sell_price - buy_price) / buy_price * 100
+                if percentage > best_percentage:
+                    best_coin_pair = coin.upper()
+                    best_percentage = percentage
+                    best_buy_exchange = buy_exchange.capitalize()
+                    best_sell_exchange = sell_exchange.capitalize()
+                    best_buy_price = buy_price
+                    best_sell_price = sell_price
+                            
+                    best_percentage = round(best_percentage, 2)
+                            
+                    best_chain_text = f"Best chain: {best_coin_pair} "
+                    best_chain_text += f"Percentage: {best_percentage}% "
+                    best_chain_text += f"bought on: {best_buy_exchange} ;"
+                    best_chain_text += f"sold on: {best_sell_exchange} ;"
+                    best_chain_text += f"Buy price: {best_buy_price} "
+                    best_chain_text += f"Sell price: {best_sell_price}"
+
+                    labelBestChainText.config(text=best_chain_text)
 
 def get_binance_price(coin):
     url = f"https://api.binance.com/api/v3/ticker/price?symbol={coin.upper()}USDT"
@@ -163,6 +182,7 @@ def get_binance_price(coin):
     sell_price = float(data_sell['price'])
     
     return f"{coin.upper()}, Binance Buy: {buy_price}, Binance Sell: {sell_price}"
+
 
 # оновлення інтерфейсу
 def update_interface():
@@ -178,7 +198,7 @@ def update_interface():
     labelCoin3.config(text=doge_info)
     labelCoin4.config(text=ltc_info)
     labelCoin5.config(text=bch_info)
-    window.after(100, update_interface)
+    window.after(1000, update_interface)
 
 update_interface()
 # запуск вікна
